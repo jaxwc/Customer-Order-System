f152be0eb1036344399751122d0e58a626abe0af
jackson <jacksonwc@proton.me>
2026-03-28 12:22:19 -0500

    added java doc

--- src/main/java/jackson/Customer-Order-System/DeliveryMethod.java
+++ src/main/java/jackson/Customer-Order-System/DeliveryMethod.java
@@ -1,13 +1,22 @@
+/** delivery options for customer orders */
 public enum DeliveryMethod {
   MAIL(3.00),
   IN_STORE_PICKUP(0.00);
 
   private final double fee;
 
+  /**
+   * creates a delivery method with the fee
+   *
+   * @param fee delivery fee
+   */
   DeliveryMethod(double fee) {
     this.fee = fee;
   }
 
+  /**
+   * @return delivery fee
+   */
   public double getFee() {
     return fee;
   }
